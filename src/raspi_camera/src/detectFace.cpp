9e480d2db9f6c288f05278026949618cb365e55d
Morita Hiromasa <morimasahirota@gmail.com>
2023-03-22 15:28:56 +0000

    complete raspi_image

--- src/raspi_camera/src/detectFace.cpp
+++ src/raspi_camera/src/detectFace.cpp
@@ -1,8 +1,8 @@
 #include <ros/ros.h>
 #include <iostream>
 #include <vector>
-#include "opencv2/opencv.hpp"
-#include "opencv2/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace cv;
 using namespace std;
--- src/raspi_image/src/detectFace.cpp
+++ src/raspi_image/src/detectFace.cpp
@@ -1,16 +1,61 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
-#include "ros/ros.h"
+#include <ros/ros.h>
 #include <iostream>
 #include <vector>
 
 using namespace std;
+//using namespace cv;
+
+
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "raspi_image");
-	bool img = cv::imwrite("/home/ubuntu/ros_ren/images/idphoto.jpg", 1); //カラーで読み込み
-	cout << "img:" << img << "\n";
+	ros::init(argc, argv, "raspi_image"); // ノード名は"raspi_image"
+	
+	cv::Mat img = cv::imread("/home/ubuntu/ros_ren/images/photo.png", 1); //カラーで読み込み
+	
+	
+	cv::CascadeClassifier cascade_classifier; // 
+	cascade_classifier.load("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"); // カスケードファイルの読み込み
+	
+
+	// カスケードファイルが読み込まれているか確認
+	if(cascade_classifier.empty())
+		cerr << "Error:cascade file not found" << endl;
+	else
+		cout << "cascade file found" << endl;
+		
+
+	vector<cv::Rect> faces; // 輪郭情報を格納する場所
+	// カスケードに基づいて顔を検出している
+	cascade_classifier.detectMultiScale(img, faces, 1.1, 3, 0,cv::Size(20, 20));
+/*
+	image:        img			--->
+	objects:      faces			--->
+	scaleFactor:  2.0			--->
+	minNeighbers: 3				--->
+	int flag: 	  0				---> もう使われないらしいけど、引数を減らすとエラーになる
+	minSize:      Size(20, 20)	--->
+*/	
+	for(int i = 0; i < faces.size(); i++)
+		rectangle(img,
+				cv::Point(faces[i].x, faces[i].y),
+				cv::Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height),
+				cv::Scalar(0, 255, 255),
+				4
+		);
+/***	rectangleの引数
+
+	img:	画像の変数
+	cv::Point(左上の点のx座標, 左上の点のy座標)
+	cv::Point(左下の点のx座標, 左下の点のy座標)
+	cv::Scale(青, 緑, 赤)
+	線の太さ
+*/
+	imwrite("/tmp/output.png", img);	// 画像を保存
+	cv::waitKey(0); // キーボードが押されるまで処理を待つ
+
 	return 0;
 
 }
