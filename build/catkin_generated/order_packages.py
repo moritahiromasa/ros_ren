68aa3ea76a9dcf7ec3a7ed6842293538bd28f29e
Morita Hiromasa <morimasahirota@gmail.com>
2023-05-23 08:05:00 +0900

    update

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/ubuntu/ros_ren/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ubuntu/ros_ren/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/ros_ren/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/ubuntu/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- src/sobel_test/src/sobel_test.cpp
+++ src/sobel_test/src/sobel_test.cpp
@@ -8,20 +8,31 @@ using namespace std;
 using namespace cv;
 
 
+
+Mat Resize_Process(Mat img)
+{
+	Mat resize_img;
+	resize(img, resize_img, Size(640, 480));
+	return resize_img;
+}
+
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "sobel_test"); // ノード名は"raspi_image"
 	
-	cv::Mat src, dst; //出力画像の行列
+	Mat src, dst; //出力画像の行列
 	
-	cv::Mat img1 = cv::imread("/home/ubuntu/ros_ren/images/img.jpg", 1);//sobel入力画像の行列
-	cv::Mat img2 = cv::imread("/home/ubuntu/ros_ren/images/img.jpg", IMREAD_GRAYSCALE);//laplaciab入力画像の行列
+	Mat img1 = cv::imread("/home/ubuntu/ros_ren/images/img.jpg", 1);//sobel入力画像の行列
+	Mat img2 = cv::imread("/home/ubuntu/ros_ren/images/img.jpg", IMREAD_GRAYSCALE);//laplaciab入力画像の行列
 	
 	if(img1.empty() || img2.empty())
 		cerr << "Error:Image file not found." << endl;
 	else
 		cout << "Image file found." << endl;
 	
+	img1 = Resize_Process(img1);
+	img2 = Resize_Process(img2);
+
 	Sobel(img1, src, -1, 0, 1);
 	Laplacian(img2, dst, 0);
 
